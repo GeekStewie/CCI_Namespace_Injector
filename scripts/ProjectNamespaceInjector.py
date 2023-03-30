74d9e4a38cdb6a2666e7adaca1757c572adfad51
Stewart Anderson <stewart.anderson@salesforce.com>
2023-03-30 13:03:28 +0100

    Update ProjectNamespaceInjector.py

--- scripts/ProjectNamespaceInjector.py
+++ scripts/ProjectNamespaceInjector.py
@@ -7,52 +7,61 @@
 # 3. Done
 #
 
+# CREATED BY: Stewart Anderson
+# DATE: NOV 7
+# USAGE: Run 'python3 scripts/AddNameSpace.py' in terminal. Note that Python v3 or above needs to be installed on local machine.
+
 import glob
-import os
 import re
+from os.path import exists
+from typing import Optional
 
 NAMESPACE_REPLACEMENT = f"%%%NAMESPACED_ORG%%%"
+USE_NAMESPACE_REPLACEMENT = True
 SEARCH_FOLDER = "force-app/main/default"
 
-# List of metadata types that do not support namespaces
-UNSUPPORTED_TYPES = {
-    'lwc': ['LightningComponentBundle'],
-    'staticresources': ['StaticResource'],
-    'contentassets': ['ContentAsset'],
-}
+def update_files(RemoveOnlyMode: Optional[bool] = False):
+  
+  # FILES SPLIT TO ALLOW FOR EASIER READING
 
-def update_files(RemoveOnly = False):
+  all_files = []
+  aura_files = glob.glob(SEARCH_FOLDER + "/aura/*.cmp", recursive=True)
+  class_files = glob.glob(SEARCH_FOLDER + "/classes/*.cls", recursive=True)
+  flow_files = glob.glob(SEARCH_FOLDER + "/flows/*.flow-meta.xml", recursive=True)
+  d_rule_files = glob.glob(SEARCH_FOLDER + "/**/*.duplicateRule-meta.xml", recursive=True)
+  vf_files = glob.glob(SEARCH_FOLDER + "/pages/*.page-meta.xml", recursive=True)
+  m_rule_files = glob.glob(SEARCH_FOLDER + "/**/*.matchingRule-meta.xml", recursive=True)
+  qa_rules = glob.glob(SEARCH_FOLDER + "/quickActions/*.quickAction-meta.xml", recursive=True)
+  lwc2_rules = glob.glob(SEARCH_FOLDER + "/lwc/**/*.js-meta.xml", recursive=True)
 
-  # Search for all metadata files in your Salesforce DX project
-  xml_files = glob.glob('force-app/main/default/**/*.xml', recursive=True)
-  cls_files = glob.glob('force-app/main/default/**/*.cls', recursive=True)
-  page_files = glob.glob('force-app/main/default/**/*.page', recursive=True)
-  metadata_files = xml_files + cls_files + page_files
-  print(f"Found {len(metadata_files)} File(s)")
+  all_files.extend(aura_files)
+  all_files.extend(class_files)
+  all_files.extend(flow_files)
+  all_files.extend(d_rule_files)
+  all_files.extend(vf_files)
+  all_files.extend(m_rule_files)
+  all_files.extend(qa_rules)
+  all_files.extend(lwc2_rules)
 
-  # Filter out metadata files that belong to unsupported types
-  supported_files = [f for f in metadata_files if all([t not in f for t in UNSUPPORTED_TYPES.get(os.path.basename(os.path.dirname(f)), [])])]
-  print(f"Found {len(supported_files)} Supported File(s)")
-
-  # Do something with the supported files, for example:
-  for f in supported_files:
+  # Process Each File Found
+  for file in all_files:
 
     # Search for all custom references within file
-    print(f"\n**Processing File**: {f}")
-    with open(f, "r") as temp_file:
+    print(f"\n**Processing File**: {file}")
+    with open(file, "r") as temp_file:
       temp_file.seek(0)
       temp_file_string = temp_file.read()
 
     # Update File String with new References
     temp_file_string = temp_file_string.replace(NAMESPACE_REPLACEMENT, "")
 
-    if not RemoveOnly:
+    if not RemoveOnlyMode:
       temp_file_string = re.sub("[\w]+__c", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
       temp_file_string = re.sub("[\w]+__r", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
       temp_file_string = re.sub("[\w]+__s", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
 
-    with open(f, "w") as tmpFile:
+    with open(file, "w") as tmpFile:
           tmpFile.write(temp_file_string)
 
 # Run Task
-update_files(False)
+update_files()
