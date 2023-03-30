81b9d20ebfcae32f343c2e6e05fd47bccf5d14d4
Stewart Anderson <stewart.anderson@salesforce.com>
2023-03-30 12:16:57 +0100

    Update ProjectNamespaceInjector.py

--- scripts/ProjectNamespaceInjector.py
+++ scripts/ProjectNamespaceInjector.py
@@ -1,52 +1,58 @@
 # CREATED BY: Stewart Anderson
-# DATE: NOV 7
-# USAGE: Note that Python v3 or above needs to be installed on local machine. Run script in terminal within VSCode window, with your CumulusCI project open.
+# CREATED DATE: 07 NOVEMBER 2022
+# USAGE:
+#
+# 1. Copy this file to the root of your CumulusCI Project Folder
+# 2. Right-click and run the file in Terminal. Note that you can set update_files(True) to just remove the namespace prefix from all files.
+# 3. Done
+#
 
 import glob
+import os
 import re
-from os.path import exists
-from typing import Optional
 
 NAMESPACE_REPLACEMENT = f"%%%NAMESPACED_ORG%%%"
-USE_NAMESPACE_REPLACEMENT = True
 SEARCH_FOLDER = "force-app/main/default"
 
-def update_files(RemoveOnlyMode: Optional[bool] = False):
-  
-  all_files = []
-  aura_files = glob.glob(SEARCH_FOLDER + "/**/*.cmp", recursive=True)
-  class_files = glob.glob(SEARCH_FOLDER + "/**/*.cls-meta.xml", recursive=True)
-  flow_files = glob.glob(SEARCH_FOLDER + "/**/*.flow-meta.xml", recursive=True)
-  d_rule_files = glob.glob(SEARCH_FOLDER + "/**/*.duplicateRule-meta.xml", recursive=True)
-  vf_files = glob.glob(SEARCH_FOLDER + "/**/*.page-meta.xml", recursive=True)
-  m_rule_files = glob.glob(SEARCH_FOLDER + "/**/*.matchingRule-meta.xml", recursive=True)
+# List of metadata types that do not support namespaces
+UNSUPPORTED_TYPES = {
+    'lwc': ['LightningComponentBundle'],
+    'staticresources': ['StaticResource'],
+    'contentassets': ['ContentAsset'],
+}
 
-  all_files.extend(aura_files)
-  all_files.extend(class_files)
-  all_files.extend(flow_files)
-  all_files.extend(d_rule_files)
-  all_files.extend(vf_files)
-  all_files.extend(m_rule_files)
+def update_files(RemoveOnly = False):
 
-  # Process Each File Found
-  for file in all_files:
+  # Search for all metadata files in your Salesforce DX project
+  xml_files = glob.glob('force-app/main/default/**/*.xml', recursive=True)
+  cls_files = glob.glob('force-app/main/default/**/*.cls', recursive=True)
+  page_files = glob.glob('force-app/main/default/**/*.page', recursive=True)
+  metadata_files = xml_files + cls_files + page_files
+  print(f"Found {len(metadata_files)} File(s)")
+
+  # Filter out metadata files that belong to unsupported types
+  supported_files = [f for f in metadata_files if all([t not in f for t in UNSUPPORTED_TYPES.get(os.path.basename(os.path.dirname(f)), [])])]
+  print(f"Found {len(supported_files)} Supported File(s)")
+
+  # Do something with the supported files, for example:
+  for f in supported_files:
 
     # Search for all custom references within file
-    print(f"\n**Processing File**: {file}")
-    with open(file, "r") as temp_file:
+    print(f"\n**Processing File**: {f}")
+    with open(f, "r") as temp_file:
       temp_file.seek(0)
       temp_file_string = temp_file.read()
 
     # Update File String with new References
     temp_file_string = temp_file_string.replace(NAMESPACE_REPLACEMENT, "")
 
-    if not RemoveOnlyMode:
+    if not RemoveOnly:
       temp_file_string = re.sub("[\w]+__c", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
       temp_file_string = re.sub("[\w]+__r", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
       temp_file_string = re.sub("[\w]+__s", f"{NAMESPACE_REPLACEMENT}\g<0>", temp_file_string)
 
-    with open(file, "w") as tmpFile:
+    with open(f, "w") as tmpFile:
           tmpFile.write(temp_file_string)
 
 # Run Task
-update_files()
+update_files(False)
